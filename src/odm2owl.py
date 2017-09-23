95e7f6b30f68d9c8b92c6c1004a29867a575389d
Alvaro <a.ortiztroncoso@tu-berlin.de>
2017-09-23 02:20:13 +0200

    Added dynamic import

--- src/odm2owl.py
+++ src/odm2owl.py
@@ -1,18 +1,18 @@
-from odm2owl.ODMSourceXMI import ODMSourceXMI
-from zargo.ZargoSource import ZargoSource
 from odm2owl.ODMModel import ODMModel
 from odm2owl.OWLSinkXSLT import OWLSinkXSLT
 
 iri = ODMModel.ns['base']
-modelPath = "test/testdata/empty.zargo"
+modelPath = "test/testdata/classes_and_properties.zargo"
 profilePath = "profiles/ODM.xmi"
 templatePath = "src/odm2owl/templates/RDF.xslt"
 savePath = "/tmp/empty.owl"
 
 try:
     if ".xmi" in modelPath:
+        from odm2owl.ODMSourceXMI import ODMSourceXMI
         model = ODMSourceXMI().loadModel(iri, modelPath, profilePath)
     elif ".zargo" in modelPath:
+        from zargo.ZargoSource import ZargoSource
         model = ZargoSource().loadModel(iri, modelPath, profilePath)
     else:
         raise Exception("Unknown input file format.")
